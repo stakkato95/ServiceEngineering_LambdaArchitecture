f8ba54bc3b41040bf22a32bca6869a75715ecbe0
Artsiom Kaliaha <stakkato95@gmail.com>
2022-05-09 16:40:58 +0200

    Ingress. k8s. Add deployment, service, ingress and scripts

---
+++ k8s/ingress/deploy/deplpoy.ps1
@@ -0,0 +1,3 @@
+kubectl apply -f .\lambda-ingress-deployment.yaml
+kubectl apply -f .\lambda-ingress-service.yaml
+kubectl apply -f .\lambda-ingress-ingress.yaml
---
+++ k8s/ingress/deploy/undeploy.ps1
@@ -0,0 +1,3 @@
+kubectl delete -f .\lambda-ingress-deployment.yaml
+kubectl delete -f .\lambda-ingress-service.yaml
+kubectl delete -f .\lambda-ingress-ingress.yaml
