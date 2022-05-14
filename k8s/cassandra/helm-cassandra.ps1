885489869c417bea8156db0c05fed9142705ed75
Artsiom Kaliaha <stakkato95@gmail.com>
2022-05-14 11:52:57 +0200

    Cassandra. Add static name for helm deployment

--- k8s/cassandra/helm-cassandra.ps1
+++ k8s/cassandra/helm-cassandra.ps1
@@ -1,5 +1,7 @@
 # https://artifacthub.io/packages/helm/bitnami/cassandra
 # github https://github.com/bitnami/charts/tree/master/bitnami/cassandra?msclkid=5f50e887d02611ecbab23bb9fb823e4f
 # why --generate-name https://github.com/bitnami/charts/issues/1838?msclkid=845a5a10d05011ec8c136ad6ca75af36
+# why cassandra-100500 https://github.com/bitnami/charts/issues/1838?msclkid=845a5a10d05011ec8c136ad6ca75af36
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install bitnami/cassandra --generate-name --set dbUser.user=cassandra --set dbUser.password=cassandra
+# helm install bitnami/cassandra --generate-name --set dbUser.user=cassandra --set dbUser.password=cassandra
+helm install cassandra-100500 bitnami/cassandra --set dbUser.user=cassandra --set dbUser.password=cassandra
